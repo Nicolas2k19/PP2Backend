42bbe464fb6ad32a81fe432f62219ab929cdd191
Maty CABJ <matias.agustin.morales@gmail.com>
2024-04-07 17:15:41 -0300

    Config de cors

---
+++ src/main/java/vdg/CorsConfig.java
@@ -0,0 +1,21 @@
+package vdg;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+	    registry.addMapping("/**")
+	            .allowedOrigins("http://192.168.0.18:8100", "http://localhost:4200")
+	            .allowedMethods("GET", "POST", "PUT", "DELETE")
+	            .allowedHeaders("*")
+	            .exposedHeaders("Authorization")
+	            .allowCredentials(true)
+	            .maxAge(3600);
+	}
+
+}
