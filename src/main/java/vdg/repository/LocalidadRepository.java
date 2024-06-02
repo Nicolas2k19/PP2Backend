37024420d5415dc9b79cc63f6de5f41a3040c0f1
Maty CABJ <matias.agustin.morales@gmail.com>
2024-06-02 20:07:37 -0300

    BUG restricciones multiples

--- src/main/java/vdg/repository/LocalidadRepository.java
+++ src/main/java/vdg/repository/LocalidadRepository.java
@@ -2,6 +2,7 @@ package vdg.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import vdg.model.domain.Localidad;
@@ -11,6 +12,7 @@ public interface LocalidadRepository extends Repository<Localidad, Integer>{
 	public List<Localidad> findAll();
 	public Localidad save(Localidad localidad);
 	public void delete(Localidad localidad);
+	@Query(value = "SELECT * FROM Localidad i WHERE i.idProvincia=?1 ", nativeQuery = true)
 	public List<Localidad> findByIdProvinciaOrderByNombreAsc(int idProvincia);
 	public Localidad findByIdLocalidad(int idLocalidad);
 }
