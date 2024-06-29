5dd74f1b3b4f3aa7ca70e4ba25efcd0a96876bec
Vanesa Vera <vanesa.vera1127@gmail.com>
2024-06-29 14:46:01 -0300

    detalle restriccion

--- src/main/java/vdg/repository/DetalleRestriccionRepository.java
+++ src/main/java/vdg/repository/DetalleRestriccionRepository.java
@@ -7,11 +7,17 @@ import org.springframework.stereotype.Repository;
 
 import vdg.model.domain.DetalleRestriccion;
 
+
 @Repository
 public interface DetalleRestriccionRepository extends JpaRepository<DetalleRestriccion, Integer> {
 
 	public DetalleRestriccion findById(int id);
 	public void deleteById(int id);
+	DetalleRestriccion findByRestriccion_IdRestriccion(int idRestriccion);
+	public void deleteByRestriccion_IdRestriccion(int idRestriccion);
+	public DetalleRestriccion save(DetalleRestriccion detalle);
+	
+	
 
 	public List<DetalleRestriccion> findAll();
 
